567b0edf966e4b51ed6d62b7ab159b512357fa4d
Siddharth Bansal <sidbansal@berkeley.edu>
2022-02-24 18:36:30 -0800

    Webcam import fixed so that the component can be used

--- src/app/app.js
+++ src/app/app.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 // const electron = window.require('electron');
 // const remote = electron.remote
 // const {BrowserWindow} = remote
-const {Webcam} = require('react-webcam');
+const Webcam = require('react-webcam');
 
 function App() {
 
     return (
         <div>
             <h1>Hello World</h1>    
-            {/* <Webcam /> */}
+            <Webcam />
         </div>        
     )
 }
